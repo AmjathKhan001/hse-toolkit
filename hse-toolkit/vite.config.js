ebcd5e80d823677b30f37af2ec6332f757b50b8f
Amjathkhan <support@resourcefulhubz.com>
2025-11-30 18:48:58 +0530

    Update vite.config.js

--- hse-toolkit/vite.config.js
+++ hse-toolkit/vite.config.js
@@ -1,9 +1,17 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path' // 1. Import 'path'
 
 export default defineConfig({
   plugins: [react()],
+  // 2. Explicitly set the root to the current directory ('.')
+  root: path.resolve(__dirname, './'), 
   build: {
+    // 3. Explicitly set the entry file
+    rollupOptions: {
+        input: 'index.html', 
+    },
     outDir: 'dist'
   }
 })
