17bc5e657f317a9cda5a0a9f0b9b4573775be47b
Amjathkhan <support@resourcefulhubz.com>
2025-11-30 19:09:29 +0530

    Create tailwind.config.js

---
+++ hse-toolkit/tailwind.config.js
@@ -0,0 +1,13 @@
+// hse-toolkit/tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    // This line tells Tailwind where to look for classes (adjust if needed)
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
